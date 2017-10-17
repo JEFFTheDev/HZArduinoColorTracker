cbea257f45c3a75ef99e7800691e9e9bb0e2685a
Jeffrey <osta0004@hz.nl>
2017-10-17 14:46:03 +0200

    Added color code detection

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,14 @@
 #include <SPI.h>
 #include <../lib/Pixy.h>
 
-void SetRandomLEDColor();
-void LogPixyBlocks();
-void WaitForSeconds(int seconds);
+void setRandomLEDColor();
+void logPixyBlocks();
+void waitForSeconds(int seconds);
+int getOffset(Block left, Block right);
 
 Pixy pixy;
+int loopDelay = 1;
+int maxAllowedOffset = 5;
 
 void setup()
 {
@@ -18,26 +21,51 @@ void setup()
 
 void loop()
 {
-  SetRandomLEDColor();
-  LogPixyBlocks();
-  WaitForSeconds(3);
+  setRandomLEDColor();
+  logPixyBlocks();
+  waitForSeconds(loopDelay);
 }
 
-void LogPixyBlocks()
+void setRandomLEDColor()
 {
-   Serial.print(pixy.getBlocks());
-}
-
-void SetRandomLEDColor()
-{
-    //TODO log color code
     int r = random(0, 255);
     int g = random(0, 255);
     int b = random(0, 255);
     pixy.setLED(r,g,b);
 }
 
-void WaitForSeconds(int seconds)
+void logPixyBlocks()
+{
+   uint16_t blocks = pixy.getBlocks();
+
+   Serial.print(blocks);
+
+   if(blocks > 0)
+   {
+     for(int i = 0; i < blocks; i++)
+     {
+       pixy.blocks[i].print();
+
+       if(blocks == 2 && pixy.blocks[0].signature != pixy.blocks[1].signature)
+       {
+         Serial.print("Found 2 signatures");
+       }
+       else if(blocks == 1)
+       {
+         if(pixy.blocks[0].signature == 1)
+         {
+           Serial.print("Green...");
+         }
+         else
+         {
+           Serial.print("Pink...");
+         }
+       }
+     }
+   }
+}
+
+void waitForSeconds(int seconds)
 {
   delay(seconds * 1000);
 }
