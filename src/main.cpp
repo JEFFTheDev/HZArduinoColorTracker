d1339eeac0608b5b947b131998ddb96f8cd19532
Jeffrey <osta0004@hz.nl>
2017-10-17 12:42:22 +0200

    Included Pixy library, added SetRandomLEDColor method, started working on registering pixy blocks

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,43 @@
 #include <Arduino.h>
 #include <SPI.h>
+#include <../lib/Pixy.h>
 
+void SetRandomLEDColor();
+void LogPixyBlocks();
+void WaitForSeconds(int seconds);
 
-void setup() {
-    // put your setup code here, to run once:
+Pixy pixy;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.print("Initiating...\n");
+
+  pixy.init();
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
+void loop()
+{
+  SetRandomLEDColor();
+  LogPixyBlocks();
+  WaitForSeconds(3);
+}
+
+void LogPixyBlocks()
+{
+   Serial.print(pixy.getBlocks());
+}
+
+void SetRandomLEDColor()
+{
+    //TODO log color code
+    int r = random(0, 255);
+    int g = random(0, 255);
+    int b = random(0, 255);
+    pixy.setLED(r,g,b);
+}
+
+void WaitForSeconds(int seconds)
+{
+  delay(seconds * 1000);
 }
