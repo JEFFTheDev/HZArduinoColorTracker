7839a3f10e43157565ba6b114f9133931ae03c3d
Thomas van de Wege <wege0014@hz.nl>
2017-10-24 10:09:11 +0200

    Removed library from lib folder

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>
 #include <SPI.h>
 
+#include <PixyUART.h>
+#include <PixySPI_SS.h>
+#include <TPixy.h>
+#include <PixyI2C.h>
+#include <Pixy.h>
 
 void setup() {
     // put your setup code here, to run once:
