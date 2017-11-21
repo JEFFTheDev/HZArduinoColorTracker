1560c0f7f9576b48eeff536d28f146130187a36c
Jeffrey van Ostayen <osta0004@hz.nl>
2017-11-21 14:41:04 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,19 @@
 
 void setRandomLEDColor();
 void logPixyBlocks();
-void drive();
+void drive(Block b);
 bool millisDelay(unsigned long millis);
+int findBlockIndexFromSignature(int signature);
 
 Pixy pixy;
 unsigned long loopDelay = 250;
 int maxAllowedOffset = 38;
-int maxX = 319;+
+int maxX = 319;
 int maxY = 199;
-unsigned long time;
+
+//123 = 83 because of decimal stuff??
+int decimalSignature = 83;
+unsigned long time = millis();
 
 MiniMoto motor0(0xC4); // A1 = 1, A0 = clear
 MiniMoto motor1(0xC0); // A1 = 1, A0 = 1 (default)
@@ -28,57 +32,59 @@ void setup()
 
 void loop()
 {
-  //setRandomLEDColor();
   if(millisDelay(loopDelay))
   {
-    logPixyBlocks();
-  }
-  //delay(1000);
-}
+    
+   int index = findBlockIndexFromSignature(decimalSignature);
 
-void setRandomLEDColor()
-{
-    int r = random(0, 255);
-    int g = random(0, 255);
-    int b = random(0, 255);
-    pixy.setLED(r,g,b);
+   if(index > -1 && pixy.blocks[index].signature == decimalSignature)
+   {
+     drive(pixy.blocks[index]);
+   }
+   else
+    {
+      motor0.stop();
+      motor1.stop();
+    }
+  }
 }
 
 // Signature 12 in Pixymon translates to 10 decimal (pixy.blocks)
 void logPixyBlocks()
 {
-   uint16_t blocks = pixy.getBlocks();
-
-   if(blocks > 0)
+   for(int i = 0; i < pixy.getBlocks(); i++)
    {
-      if(pixy.blocks[0].signature == 10)
-      {
-        pixy.blocks[0].print();
-        //drive();
-        drive();
-      }
-     }
-   else
-      {
-        motor0.stop();
-        motor1.stop();
-      }
+      pixy.blocks[i].print();
+   }
 }
 
-void drive()
-{
-   bool left = pixy.blocks[0].x > ((maxX / 2) + maxAllowedOffset);
-   bool right = pixy.blocks[0].x < ((maxX / 2) - maxAllowedOffset);
 
-   int x = pixy.blocks[0].x;
+int findBlockIndexFromSignature(int signature)
+{
+   for (int i=0; i< pixy.getBlocks(); i++)
+   {
+      if(pixy.blocks[i].signature == signature)
+      {
+         return i;
+      }
+   }
+   return -1;
+}
+
+void drive(Block b)
+{
+   bool left = b.x > ((maxX / 2) + maxAllowedOffset);
+   bool right = b.x < ((maxX / 2) - maxAllowedOffset);
+
+   int x = b.x;
    x = right ? 319 - x : x;
 
    int speed = map(x,0,319,0,15);
 
    Serial.println(speed);
 
-   int motor0Speed = left ? 25 + speed : 20;
-   int motor1Speed = right ? 25 + speed : 20;
+   int motor0Speed = left ? 18 + speed : 20;
+   int motor1Speed = right ? 18 + speed : 20;
 
    motor0.drive(motor0Speed);
    motor1.drive(motor1Speed);
@@ -87,8 +93,8 @@ void drive()
 //seconds is too slow so it's waiting for some random integer
 bool millisDelay(unsigned long milliSeconds)
 {
-   unsigned long millisArduino = millis();
-   if(millisArduino > time +  milliSeconds)
+
+   if(millis() > time +  milliSeconds)
    {
      time = millis();
      Serial.println("Seconds past...");
